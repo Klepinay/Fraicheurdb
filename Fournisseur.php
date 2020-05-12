7681f16dec31ff009d6166aa0a7e2c1c311f06c0
Klepinay <kevin@lepinay.re>
2020-05-12 12:05:49 +0400

    Create page Fournisseur + featured create and import fournisseur

--- Fournisseur.php
+++ Fournisseur.php
@@ -18,5 +18,60 @@
 			</ul>
 		</nav>
 	</div>
+	
+				<button value="Ajouter un Fournisseur" id="ajout" style="margin-top: 25px; margin-left: 200px" >+ Ajouter un Fournisseur</button><br /><br />
+				<div id="form">
+				<h1>Ajouter un Fournisseur</h1>
+					<form method="post" action="Ajoutfour.php">
+						<p><label for="name">Nom du Fournisseur : </label> <input type="text" name="name" id="name"> </p>
+						<p><label for="code">Code Fournisseur : </label> <input type="text" name="code" id="code"> </p> 
+						<p><input type="submit" value="Ajouter" /></p>
+					</form>
+					
+					<form enctype="multipart/form-data" action="importfournisseur.php" method="post">
+						<div class="input-row">
+							<label class="col-md-4 control-label">Choisir un fichier CSV</label>
+							<input type="file" name="file" id="file" accept=".csv">
+							<button type="submit" id="submit" name="import" class="btn-submit">Import</button>
+							<br />
+						</div>
+					</form>
+				</div>
+				
+				<table width="50%" border="1" style="border-collapse:collapse" align="center">
+				<tr align="center">
+					<td>
+						<p>Nom du Fournisseur</p>
+					</td>
+					<td>
+						<p>Code Fournisseur</p>
+					</td>
+				</tr>
+		<?php 
+			$bdd = new PDO('mysql:host=localhost;dbname=fraicheurdb;charset=utf8', 'root', '');
+			$fournisseurs = $bdd->query('select * from t_fournisseur order by frn_id');
+			foreach ($fournisseurs as $fournisseur): 
+		?>
+				<tr align="center">
+					<td>
+						<?php echo $fournisseur['frn_name'] ?>
+					</td>
+					<td>
+						<?php echo $fournisseur['frn_code'] ?>
+					</td>
+					<td></td>
+				</tr>
+		<?php endforeach ?>
+				
+			</table>
 </body>
 </html>
+<script type="text/javascript">
+	document.querySelector("#ajout").onclick = function() {
+		if (window.getComputedStyle(document.querySelector('#form')).display=='block'){
+			document.querySelector("#form").style.display="none";
+		} else {
+			document.querySelector("#form").style.display="block";
+		}
+	}
+</script>
